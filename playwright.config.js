4da728ea5a19197ea4754694e0622c5e3d87f3e3
Pedro Costa <pedro.costa@anota.ai>
2025-12-07 01:46:32 -0300

    Ajuste plugin do QASE

--- playwright.config.js
+++ playwright.config.js
@@ -1,36 +1,34 @@
-
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const hasQaseToken = !!process.env.QASE_TESTOPS_API_TOKEN;
+
 export default defineConfig({
-  testDir: 'e2e/tests',
+  testDir: './e2e/tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   reporter: [
-    ['list'], // Adiciona o reporter list para mostrar logs no console
+    ['list'],
     ['html'],
-    ['playwright-qase-reporter', {
-      mode: 'testops',
-      debug: false,
-      testops: {
-        api: {
-          token: process.env.QASE_TESTOPS_API_TOKEN,
-        },
-        project: process.env.QASE_TESTOPS_PROJECT,
-        uploadAttachments: true,
-        run: {
-          complete: true,
-        },
-        batch: {
-          size: 1,
+    ...(hasQaseToken ? [[
+      'playwright-qase-reporter',
+      {
+        mode: 'testops',
+        debug: false,
+        testops: {
+          api: { token: process.env.QASE_TESTOPS_API_TOKEN },
+          project: process.env.QASE_TESTOPS_PROJECT,
+          uploadAttachments: true,
+          run: { complete: true },
+          batch: { size: 1 },
         },
       },
-    }],
+    ]] : []),
   ],
   use: {
     trace: 'on-first-retry',
@@ -44,3 +42,4 @@ export default defineConfig({
     },
   ],
 });
+
